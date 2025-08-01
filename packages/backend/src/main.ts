201836504bcd8d48c6da6fcf5890b1f008ce41cd
Anastasia M <morozanastasia2005@gmail.com>
2025-08-01 14:46:15 +0300

    feat: add OAuth callback page and integrate Google/Github login functionality

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -24,6 +24,6 @@ async function bootstrap() {
 
   app.useStaticAssets(join(__dirname, '..', 'uploads'), { prefix: '/uploads/' });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
