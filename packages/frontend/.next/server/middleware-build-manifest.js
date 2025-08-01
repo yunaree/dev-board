790486cff3968342c5a5ae3de967314b27a1a68e
Anastasia M <morozanastasia2005@gmail.com>
2025-08-01 22:38:14 +0300

    Refactor code structure for improved readability and maintainability

--- packages/frontend/.next/server/middleware-build-manifest.js
+++ packages/frontend/.next/server/middleware-build-manifest.js
@@ -1,6 +1,21 @@
 globalThis.__BUILD_MANIFEST = {
   "pages": {
-    "/_app": []
+    "/_app": [
+      "static/chunks/[root-of-the-server]__e2c08166._.js",
+      "static/chunks/node_modules_react-dom_82bb97c6._.js",
+      "static/chunks/node_modules_a51498a5._.js",
+      "static/chunks/[root-of-the-server]__9fb8b85a._.js",
+      "static/chunks/packages_frontend_pages__app_5771e187._.js",
+      "static/chunks/packages_frontend_pages__app_6120854a._.js"
+    ],
+    "/_error": [
+      "static/chunks/[root-of-the-server]__8df7605f._.js",
+      "static/chunks/node_modules_react-dom_82bb97c6._.js",
+      "static/chunks/node_modules_a51498a5._.js",
+      "static/chunks/[root-of-the-server]__cfb10835._.js",
+      "static/chunks/packages_frontend_pages__error_5771e187._.js",
+      "static/chunks/packages_frontend_pages__error_dfa048ef._.js"
+    ]
   },
   "devFiles": [],
   "ampDevFiles": [],
